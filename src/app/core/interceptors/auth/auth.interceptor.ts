a673f5e02b7b0599346b3a1b2fa53f9a2ebb87a4
Danilo Teixeira <danilo.rt@outlook.com>
2023-06-27 11:19:26 -0300

    Ajuste interceptor

--- src/app/core/interceptors/auth/auth.interceptor.ts
+++ src/app/core/interceptors/auth/auth.interceptor.ts
@@ -5,31 +5,25 @@ import {
   HttpEvent,
   HttpInterceptor,
 } from '@angular/common/http';
-import { first, Observable, switchMap } from 'rxjs';
-import { AuthService } from 'src/app/features/auth/services/auth';
+import { Observable } from 'rxjs';
 import { StorageService } from '../../services/storage';
 
 @Injectable()
 export class AuthInterceptor implements HttpInterceptor {
   private storageService = inject(StorageService);
-  private isAuthenticated$ = inject(AuthService).isAuthenticated$;
 
   intercept(
     request: HttpRequest<unknown>,
     next: HttpHandler
   ): Observable<HttpEvent<unknown>> {
-    this.isAuthenticated$.subscribe((isAuthenticated) => {
-      if (isAuthenticated) {
-        const accessToken = this.storageService.getItem('access_token');
-        request = request.clone({
-          setHeaders: { Authorization: `Bearer ${accessToken}` },
-        });
-      }
-    });
+    const accessToken = this.storageService.getItem('access_token');
 
-    return this.isAuthenticated$.pipe(
-      first(),
-      switchMap(() => next.handle(request))
-    );
+    if (accessToken) {
+      request = request.clone({
+        setHeaders: { Authorization: `Bearer ${accessToken}` },
+      });
+    }
+
+    return next.handle(request);
   }
 }
